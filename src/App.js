d4c4b1ea4a346bc137fd89bf6a3b37ae2246e0d8
benalper2 <ben_alper@live.com>
2021-12-27 14:28:03 +0000

    App.js Text Changed main page

--- src/App.js
+++ src/App.js
@@ -1,3 +1,12 @@
+export default App;
+
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('Hello World!');
+}).listen(8080);
+
+/*
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
@@ -11,6 +20,4 @@ function App() {
       </header>
     </div>
   );
-}
-
-export default App;
+}*/
