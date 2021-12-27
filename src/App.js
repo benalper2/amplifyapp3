6450d7782b4ec97f0db101f13715beee033b23b1
benalper2 <ben_alper@live.com>
2021-12-27 15:29:06 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,4 @@
-export default App;
 
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
-}).listen(8080);
-
-/*
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
@@ -16,8 +8,17 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
+        <h1>Hello from V3</h1>
       </header>
     </div>
   );
-}*/
+}
+
+/*
+export default App;
+
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('Hello World!');
+}).listen(8080);*/
